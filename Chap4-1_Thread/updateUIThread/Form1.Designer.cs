752599830c74378f39dea216ceabbd86faac6634
The space of Qratosone <qratosone@live.com>
2016-06-28 01:18:10 +0800

    updateUIThread

---
+++ Chap4-1_Thread/updateUIThread/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace updateUIThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtArgs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "跨线程更新";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(124, 227);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "更新";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtArgs
+            // 
+            this.txtArgs.Location = new System.Drawing.Point(238, 229);
+            this.txtArgs.Name = "txtArgs";
+            this.txtArgs.Size = new System.Drawing.Size(100, 21);
+            this.txtArgs.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 347);
+            this.Controls.Add(this.txtArgs);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtArgs;
+    }
+}
+
---
+++ Chap4-1_Thread/updateUIThread/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace updateUIThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ThreadMethod(Object info)
+        {
+            Action del = delegate ()//Action 表示无参，无返回值的委托
+            {
+                label1.Text = "hello";
+            };
+            Action<string> del2 = delegate (string infovalue)
+            {
+                label1.Text = infovalue;
+            };
+            label1.Invoke(del);
+            Thread.Sleep(2000);
+            label1.Invoke(del2, new object[] { info });
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Thread th1 = new Thread(ThreadMethod);
+            th1.Start(txtArgs.Text);
+        }
+    }
+}
